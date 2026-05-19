28e09964858c05875e1adac6b1c76ac72b2c71fa
Omkar Kokane <omkarrajeshkokane@gmail.com>
2026-05-19 16:07:18 +0200

    DIMC Instruction Optimization with variable

--- pulp/ara/ara_v2.py
+++ pulp/ara/ara_v2.py
@@ -79,6 +79,7 @@ def attach(component: Component, vlen: int, nb_lanes: int, use_spatz: bool=False
     if use_spatz:
         component.add_sources([
             "cpu/iss_v2/src/cores/spatz/spatz_vlsu.cpp",
+            "cpu/iss_v2/src/cores/spatz/dimc.cpp",
         ])
         component.add_c_flags([
             "-DCONFIG_GVSOC_ISS_USE_SPATZ",
